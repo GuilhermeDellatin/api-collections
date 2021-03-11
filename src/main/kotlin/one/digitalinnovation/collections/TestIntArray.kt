204e09c959b945aa7d38abf4e0973a2e5eaadf1b
GuilhermeDellatin <guidellatin@gmail.com>
2021-03-11 20:44:23 -0300

    Arrays e intArrayOf

---
+++ src/main/kotlin/one/digitalinnovation/collections/TestIntArray.kt
@@ -0,0 +1,36 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values) {
+        println(valor)
+    }
+    println("---------------")
+
+    //Já está utilizando um recurso de expressão Lambda, com it
+    values.forEach {
+        println(it)
+    }
+    println("---------------")
+
+    values.forEach { valor ->
+        println(valor)
+    }
+    println("---------------")
+
+    for (index in values.indices) {
+        println(values[index])
+    }
+    println("---------------")
+    values.sort()
+    for (valor in values) {
+        println(valor)
+    }
+}
